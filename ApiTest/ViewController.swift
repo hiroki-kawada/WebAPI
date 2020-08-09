89ad02e439d35e69deade2ea492f86a3af6cdd68
h-kawada <f13bc005@gmail.com>
2020-08-10 08:04:37 +0900

    Initial Commit

---
+++ ApiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApiTest
+//
+//  Created by 川田広輝 on 2020/08/10.
+//  Copyright © 2020 川田広輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
